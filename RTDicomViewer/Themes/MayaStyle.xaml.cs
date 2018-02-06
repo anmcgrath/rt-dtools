41875d335397237052b1e0e9cd9eba1d25e94db4
Andrew <Andrew@ANDREW-SBOOK>
2018-02-06 13:40:01 +1100

    maya theme added

---
+++ RTDicomViewer/Themes/MayaStyle.xaml.cs
@@ -0,0 +1,36 @@
+﻿// (C) Copyright 2014 by Autodesk, Inc.
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+
+//- Written by Cyrille Fauvel, Autodesk Developer Network (ADN)
+//- http://www.autodesk.com/joinadn
+//- December 30th, 2013
+//
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace Autodesk.Maya {
+
+	public partial class MayaStyle : ResourceDictionary {
+
+		public MayaStyle () {
+			InitializeComponent () ;
+		}
+
+	}
+
+}
